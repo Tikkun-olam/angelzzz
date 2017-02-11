713b4abe428ddfeea239b3810dd11ab57035a27a
Guy Sheffer <guysoft@gmail.com>
2017-02-11 22:47:42 +0000

    Add Orvibo S20 installation deps

--- src/install_deps.sh
+++ src/install_deps.sh
@@ -2,5 +2,6 @@ sudo apt-get update
 sudo apt-get install -y python-bluez python-numpy python-pip mysql-server mysql-client python-mysqldb
 sudo pip install SQLAlchemy
 sudo pip install mysql-connector
+sudo pip install https://github.com/guysoft/OrviboS20/archive/master.zip
 sudo pip install flask
 sudo pip install flask-bootstrap
