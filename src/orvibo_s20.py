04ed1a30c379f9fa426b2adb1f42db10e972e5e3
Guy Sheffer <guysoft@gmail.com>
2017-02-16 11:54:20 +0000

    Change it so we use system calls and not the S20control lib directly, because this makes better garbage collection for socket

--- src/orvibo_s20.py
+++ src/orvibo_s20.py
@@ -1,21 +1,26 @@
 import time
-from S20control import orviboS20
+#from S20control import orviboS20
 from common import iniToDict, CONFIG_PATH
+import os
 
 settings = iniToDict(CONFIG_PATH)
+failcount = 0
+#power_socket = orviboS20()
 
 def on():
-    power_socket = orviboS20()
-    power_socket.poweron(settings["beddit"]["relay_ip"], settings["beddit"]["relay_mac"])
+    #power_socket.poweron(settings["beddit"]["relay_ip"], settings["beddit"]["relay_mac"])
+    os.system("S20control poweron " + settings["beddit"]["relay_ip"] + " " + settings["beddit"]["relay_mac"])
 
 def off():
-    power_socket = orviboS20()
-    power_socket.poweroff(settings["beddit"]["relay_ip"], settings["beddit"]["relay_mac"])
+    # power_socket.poweroff(settings["beddit"]["relay_ip"], settings["beddit"]["relay_mac"])
+    os.system("S20control poweroff " + settings["beddit"]["relay_ip"] + " " + settings["beddit"]["relay_mac"])
 
 def restart():
     off()
-    time.sleep(2)
+    time.sleep(3)
     on()
+            
+            
 
 if __name__ == "__main__":
     restart()
